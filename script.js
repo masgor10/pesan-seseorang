545764c7c0f3f63c5508bccf02778cef2c12ba44
masgor10 <bosokersgaming@gmail.com>
2025-11-27 06:04:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBL5ZPaGdX0GxItbuICKEDV3SoyIUvKxvo",
+  authDomain: "pesan-yang-tak-tersampaikan.firebaseapp.com",
+  projectId: "pesan-yang-tak-tersampaikan",
+  storageBucket: "pesan-yang-tak-tersampaikan.firebasestorage.app",
+  messagingSenderId: "272593176982",
+  appId: "1:272593176982:web:56367dc306cf42c7ac75b7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
